1b8d5aeeea2e87fa019d5df1a3a30d536fa23179
Ivan <ivanjimenezvalencia4@gmail.com>
2024-05-07 18:45:36 +0200

    pruebas para calcular el adx

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import pandas as pd
+
 import dates
+import indicadores
 import tester
 
 if __name__ == '__main__':
     symbol = "RUNE-USDT"
     temporality = "30m"
-    smaValues = [18,19,20,21,22,23,24,25,26,27]
-    emaValues = [30,31,32,33,34,35,36,37,38,39,40]
-    takeProfitValues = [4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5]
+    smaValues = [25]
+    emaValues = [34]
+    takeProfitValues = [8]
 
     fechas = dates.get_all_data()
     initialCapital = 1000
     #tester.tester(symbol, temporality, smaValues, emaValues, takeProfitValues, fechas, initialCapital)
-
-    for fecha in fechas:
-        data = tester.get_pd_data_market(symbol, fecha, temporality)
+    data = pd.read_csv("resources/RUNE-USDT/all_data.csv", sep=',')
+    totalLength = len(data)
+    for indice in range(len(data) - 1, -1, -1):
+        if (totalLength - indice) > 27:
+            dataRecorrida = data.tail(totalLength - indice)
+            adx = indicadores.calculate_adx(dataRecorrida, 14)
+    a = 1
