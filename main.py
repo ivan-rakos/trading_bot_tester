540a7d61c438dc712e1b8624a185f120fa4d81c6
javiersmartin <javiersmartin@ext.inditex.com>
2024-06-11 20:27:22 +0200

    try calculator

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import logging
+
 import pandas as pd
 
+import adxCalculator
 import dates
 import indicadores
 import tester
@@ -14,10 +17,15 @@ if __name__ == '__main__':
     fechas = dates.get_all_data()
     initialCapital = 1000
     #tester.tester(symbol, temporality, smaValues, emaValues, takeProfitValues, fechas, initialCapital)
-    data = pd.read_csv("resources/RUNE-USDT/all_data.csv", sep=',')
-    totalLength = len(data)
-    for indice in range(len(data) - 1, -1, -1):
-        if (totalLength - indice) > 27:
-            dataRecorrida = data.tail(totalLength - indice)
-            adx = indicadores.calculate_adx(dataRecorrida, 14)
-    a = 1
+    data = pd.read_csv("resources/RUNE-USDT/2024-05_30m.csv", sep=',')
+    close = data['Close']
+    high = data['high']
+    low = data['low']
+    adx = adxCalculator.adx(close, high, low, 34)
+   
+    #totalLength = len(data)
+    #for indice in range(len(data) - 1, -1, -1):
+    #    if (totalLength - indice) > 27:
+    #        dataRecorrida = data.tail(totalLength - indice)
+    #        adx = indicadores.calculate_adx(dataRecorrida, 14)
+    #a = 1
